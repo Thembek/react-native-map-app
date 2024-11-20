3c283e0e949209121e2a6706f8f2dc0fcca31488
Thembek <andile.goba@younglings.africa>
2024-11-20 14:46:49 +0200

    first commit

---
+++ screens/AllPlaces.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import { useIsFocused } from '@react-navigation/native';
+
+import PlaceList from '../components/Places/PlacesList';
+import { fetchPlaces } from '../util/database';
+
+
+function AllPlaces({ route }){
+    const [loadedPlaces, setLoadedPlaces] = useState();
+
+    const isFocused = useIsFocused();
+
+    useEffect(() => {
+        async function loadPlaces(){
+            const places = await fetchPlaces();
+            setLoadedPlaces(places);
+        }
+        if(isFocused){
+            loadPlaces();
+            //setLoadedPlaces(curPlaces => [...curPlaces, route.params.place]);
+        }
+    }, [isFocused]);
+
+    return(
+        <PlaceList places={loadedPlaces}/>
+    )
+}
+
+
+export default AllPlaces;
